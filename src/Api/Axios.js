597531b3691c4b29d982e272c7b0bc58b07b1130
Aviinaaash <aviinaashdevkate@gmail.com>
2023-05-08 16:47:29 +0530

    axios  upddted

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseURL="https://my-machine-store-restapi.onrender.com"
-const baseURL = "http://localhost:5000"
-// const baseURL = "http://15.206.68.236:5001"
+// const baseURL = "http://localhost:5000"
+const baseURL = "http://15.207.31.23:5001"
 
 export default axios.create({ baseURL:  baseURL});
