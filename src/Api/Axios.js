0f9f7494c9a2930641eac4bf6bb1a5c4fc6ffc69
omtapse <omtapse01@gmail.com>
2023-07-10 18:04:45 +0530

    changing in styling

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const baseURL="https://my-machine-store-restapi.onrender.com"
-// const baseURL = "http://194.31.53.145:5000/api"
+const baseURL = "http://194.31.53.145:5000/api"
 // const baseURL = "http://15.207.31.23:5000"
-const baseURL = "http://localhost:5000"
+// const baseURL = "http://localhost:5000"
 
-export default axios.create({ baseURL:  baseURL});
+export default axios.create({ baseURL:  baseURL,});
